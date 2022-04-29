9d74a3b7d2bbbc3be5af5ba4fb5b402d8b9b9dd3
Sathya <sathyaprakash.sp.6@gmail.com>
2022-04-29 17:25:17 +0530

    passenger counter app updated

--- 01.Passanger Counter App/index.js
+++ 01.Passanger Counter App/index.js
@@ -5,13 +5,16 @@
 // let myDogAge = myAge * humanDogRatio;
 // console.log(myDogAge);
 let counterEle = document.getElementById("count-el");
+const entries = document.getElementById("prev-entries");
+
 let count = 0;
 const increase = () => {
   count += 1;
-  counterEle.innerHTML = count;
-}
+  counterEle.textContent = count;
+};
 
-const  save = () => {
-  const entries = document.getElementById("prev-entries");
+const save = () => {
   entries.innerHTML += `<li>${count}</li>`;
-}
+  counterEle.textContent = 0;
+  count = 0;
+};
