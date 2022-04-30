216b07bea32d3253730111e25168bfdec13c7619
Sathya <sathyaprakash.sp.6@gmail.com>
2022-04-30 17:59:58 +0530

    blackjack game practice

--- 02.Blackjack Game/index.js
+++ 02.Blackjack Game/index.js
@@ -1,5 +1,3 @@
-
-
 // 1. create two variable, firstCard & secondCard
 // set their values to random number between 2 - 11
 
@@ -11,3 +9,22 @@ console.log(firstCard, secondCard);
 
 let sum = firstCard + secondCard;
 console.log(sum);
+
+if (sum < 21) {
+  console.log("Do you want to draw a card?!");
+} else if (sum === 21) {
+  console.log(" Whooo You win!");
+} else {
+  console.log(" you're ouit of game");
+}
+
+// check if the person is old enough to play the game(21)
+//Log a suitable message to the console in both the cases
+
+let age = Math.random() * 100;
+
+if(age >= 21){
+  console.log("You can play the game");
+} else {
+  console.log("You can't play the game");
+}
