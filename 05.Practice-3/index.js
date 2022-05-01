f49cd509dc61f9e94b783bd7a6b664cc5a3269c5
Sathya <sathyaprakash.sp.6@gmail.com>
2022-05-01 20:05:07 +0530

    practice template literals

---
+++ 05.Practice-3/index.js
@@ -0,0 +1,14 @@
+// template strings/ literals
+
+const recipient = "James";
+
+// Refactor the code below to use template strings
+
+const email = "Hey " + recipient + "! how it going ?";
+console.log(email);
+
+const emailTemplate = `Hey ${recipient}! how it going ?`;
+console.log(emailTemplate);
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
