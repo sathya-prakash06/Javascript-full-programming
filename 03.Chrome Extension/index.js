f56c81c774c6c1abda709d68c491159a486e1b90
Sathya <sathyaprakash.sp.6@gmail.com>
2022-05-01 20:00:32 +0530

    chrome extension updated

--- 03.Chrome Extension/index.js
+++ 03.Chrome Extension/index.js
@@ -1,7 +1,24 @@
-const myLead = [];
+let myLead = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
-  console.log("Button clicked");
+  myLead.push(inputEl.value);
+  console.log(myLead);
+  inputEl.value = "";
+  renderLeads();
 });
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLead.length; i++) {
+    listItems += `<li><a href= ${myLead[i]}' target='_blank'>${myLead[i]}</a></li>`;
+    // console.log(listItems);
+    // another method
+    // const liEl = document.createElement("li");
+    // liEl.textContent = myLead[i];
+    // ulEl.appendChild(liEl);
+  }
+  ulEl.innerHTML = listItems;
+}
