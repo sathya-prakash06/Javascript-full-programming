e2be42e5a275f9a3aa770de5cb74927691fe78cd
Sathya <sathyaprakash.sp.6@gmail.com>
2022-05-01 21:51:16 +0530

    chrome extensions added

--- 03.Chrome Extension/index.js
+++ 03.Chrome Extension/index.js
@@ -1,24 +1,46 @@
-let myLead = [];
+// chrome://extensions/
+
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const dltBtn = document.getElementById("dlt-btn");
 
-inputBtn.addEventListener("click", function () {
-  myLead.push(inputEl.value);
-  console.log(myLead);
-  inputEl.value = "";
+//localStorage.setItem('myLead', 'www.google.com');
+let myLead = JSON.parse(localStorage.getItem("myLeads"));
+
+if (myLead) {
   renderLeads();
-});
+}
 
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLead.length; i++) {
-    listItems += `<li><a href= ${myLead[i]}' target='_blank'>${myLead[i]}</a></li>`;
+    listItems += `
+    <li>
+    <a target='_blank' href= '${myLead[i]}' >${myLead[i]}</a>
+    </li>`;
+    console.log(myLead[i]);
     // console.log(listItems);
     // another method
     // const liEl = document.createElement("li");
     // liEl.textContent = myLead[i];
     // ulEl.appendChild(liEl);
   }
+
+  inputBtn.addEventListener("click", function () {
+    myLead.push(inputEl.value);
+    console.log(myLead);
+    localStorage.setItem("myLeads", JSON.stringify(myLead));
+    inputEl.value = "";
+    renderLeads();
+  });
+
   ulEl.innerHTML = listItems;
 }
+
+dltBtn.addEventListener("click", function () {
+  //console.log('clicked')
+  localStorage.removeItem("myLeads");
+  myLead = [];
+  renderLeads();
+});
