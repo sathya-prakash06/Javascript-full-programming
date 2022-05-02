82a8c702bb872a43d85613b0bd74a7b9f2ab7276
Sathya <sathyaprakash.sp.6@gmail.com>
2022-05-02 10:45:05 +0530

    chrome extension updated

--- 03.Chrome Extension/index.js
+++ 03.Chrome Extension/index.js
@@ -1,27 +1,29 @@
 // chrome://extensions/
-
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+
 const dltBtn = document.getElementById("dlt-btn");
+let tabBtn = document.getElementById("tab-btn");
 
 //localStorage.setItem('myLead', 'www.google.com');
-let myLead = [];
+
 let leadsLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsLocalStorage) {
-  myLead = leadsLocalStorage
-  renderLeads();
+  myLeads = leadsLocalStorage;
+  renderLeads(myLeads);
 }
 
-function renderLeads() {
+function renderLeads(leads) {
   let listItems = "";
-  for (let i = 0; i < myLead.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
     <li>
-    <a target='_blank' href= 'https://${myLead[i]}' >${myLead[i]}</a>
+    <a target='_blank' href= 'https://${leads[i]}' >${leads[i]}</a>
     </li>`;
-    console.log(myLead[i]);
+    console.log(leads[i]);
     // console.log(listItems);
     // another method
     // const liEl = document.createElement("li");
@@ -29,20 +31,33 @@ function renderLeads() {
     // ulEl.appendChild(liEl);
   }
 
-  inputBtn.addEventListener("click", function () {
-    myLead.push(inputEl.value);
-    console.log(myLead);
-    localStorage.setItem("myLeads", JSON.stringify(myLead));
-    inputEl.value = "";
-    renderLeads();
-  });
-
   ulEl.innerHTML = listItems;
 }
 
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  
+  renderLeads(myLeads);
+});
+
+// delete button
 dltBtn.addEventListener("click", function () {
   //console.log('clicked')
   localStorage.clear();
-  myLead = [];
-  renderLeads();
+  myLeads = [];
+  renderLeads(myLeads);
+});
+
+// Tab button
+tabBtn.addEventListener("click", function () {
+  console.log("clicked");
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    //console.log(tabs)
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    renderLeads(myLeads);
+  });
 });
