9c498ca004f859082bfe678856369f828dc4ff2c
Sathya <sathyaprakash.sp.6@gmail.com>
2022-05-02 10:10:46 +0530

    practice files updated

--- 05.Practice-3/index.js
+++ 05.Practice-3/index.js
@@ -25,25 +25,39 @@ greetUser("James", "😍");
 
 console.log(addTwoNumbers(1, 2)); // 3
 
-function addTwoNumbers(a, b) { // parameters
+function addTwoNumbers(a, b) {
+  // parameters
   return a + b;
 }
 
 console.log(addTwoNumbers(87, 1)); // 88  // arguments
 
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 
 // create a function , getFirst(arr) , that returns the first element of an array
 
-function getFirst(arr){
-    return arr[0];
+function getFirst(arr) {
+  return arr[0];
 }
 
-let arr = [1,2,3,4,5];
+let arr = [1, 2, 3, 4, 5];
 
 console.log(getFirst(arr)); // 1
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let myCourses = ["HTML", "CSS", "JS"];
+
+// create a function that takes a single parameter, an array
+// and logs all the itemns of the array
+// call the function with the array above
+
+function coursesFn(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+coursesFn(myCourses);
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
